a400a03bf3970f8c7fd632d4b19d67a13f04b464
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-02-03 14:57:55 -0500

    Added more components and nav

---
+++ client/src/components/Nav.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../images/google-books-logo.png"
+import '../App.css';
+
+
+function Nav() {
+    return (
+        
+
+    <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+    {/* <Link to="/pages/SearchPage"
+                            className={
+                                window.location.pathname === "/pages/SearchPage" || window.location.pathname === "/home"
+                                    ? "nav-link active"
+                                    : "navbar-brand"
+                            }>
+                                <img src={Logo} id="logo" class="navbar-brand" alt="logo"/>
+            </Link>  */}
+     
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+            <Link  to="/pages/SearchPage"
+                            className={window.location.pathname === "/pages/SearchPage" ? "nav-link active" : "nav-link"}
+                        >
+                            Search Books
+            </Link>
+              
+            </li>
+            <li className="nav-item">
+            <Link  to="/pages/SavePage"
+                            className={window.location.pathname === "/pages/SavePage" ? "nav-link active" : "nav-link"}
+                        >
+                            Saved Books
+            </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+//  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+//   <div class="container-fluid">
+//     <a class="navbar-brand" href="#">Navbar</a>
+//     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//       <span class="navbar-toggler-icon"></span>
+//     </button>
+//     <div class="collapse navbar-collapse" id="navbarNav">
+//       <ul class="navbar-nav">
+//         <li class="nav-item">
+//           <a class="nav-link active" aria-current="page" href="#">Home</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href="#">Features</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href="#">Pricing</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+//         </li>
+//       </ul>
+//     </div>
+//   </div>
+// </nav>
+
+    );
+}
+
+export default Nav;
