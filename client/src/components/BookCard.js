889ce9fe9d990df9a3adf4652ea25ac08500ae95
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-02-05 23:34:32 -0500

    added readme, reverted my card and boolList back to the way it was. Going to try to tackle the button functionality

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -1,48 +1,48 @@
-// import React from 'react';
-// import '../App.css';
-// import Row from "../components/Row.js";
-// import Col from "../components/Col.js";
+import React from 'react';
+import '../App.css';
+import Row from "../components/Row.js";
+import Col from "../components/Col.js";
 
 
-// const BookCard = (props) => {
+const BookCard = (props) => {
   
   
-// //  renders out all of the book cards
-//   return (
+//  renders out all of the book cards
+  return (
 
-// <Row className="row">
-//  <Col className="col" size="sm-12">
+<Row className="row">
+ <Col className="col" size="sm-12">
 
-// <div className="card mb-3">
-// <div className="row no-gutters">
-//   <div className="col-md-4">
-//   <img src={props.image} className="card-img" alt="" />
-//   </div>
-//   <div className="col-md-8">
-//     <div className="card-body">
-//       <h5 className="card-title">{props.title}</h5>
-//       <p className="card-text">Author: {props.author}</p>
-//       <p className="card-text">{props.description}</p>
-//       <div id="wrapper">
-//       <button className="saveBook btn btn-primary" id={props.id} onClick={props.handleSavedButton}> 
-//                                             Save Book
-//                                         </button>
-//                                         <a href={props.view} target="_blank" />
-//                                             <button className="viewBook btn btn-success">
-//                                                 View Book
-//                                         </button>
+<div className="card mb-3">
+<div className="row no-gutters">
+  <div className="col-md-4">
+  <img src={props.image} className="card-img" alt="" />
+  </div>
+  <div className="col-md-8">
+    <div className="card-body">
+      <h5 className="card-title">{props.title}</h5>
+      <p className="card-text">Author: {props.author}</p>
+      <p className="card-text">{props.description}</p>
+      <div id="wrapper">
+      <button className="saveBook btn btn-primary" id={props.id} onClick={props.handleSavedButton}> 
+                                            Save Book
+                                        </button>
+                                        <a href={props.view} target="_blank" />
+                                            <button className="viewBook btn btn-success">
+                                                View Book
+                                        </button>
     
-//     </div>
-//   </div>
-// </div>
-// </div>
+    </div>
+  </div>
+</div>
+</div>
 
-// </div>
+</div>
 
-// </Col>
-// </Row>
+</Col>
+</Row>
     
-//   );
-// }
+  );
+}
 
-// export default BookCard;
+export default BookCard;
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,49 +1,14 @@
 import React from 'react';
 import '../App.css';
-// import BookCard from './BookCard.js';
-import Container from './Container';
-import Row from "../components/Row.js";
-import Col from "../components/Col.js";
+import BookCard from './BookCard.js';
+// import Row from "../components/Row.js";
+// import Col from "../components/Col.js";
 
 const BookList = (props) => {
   console.log(props.books);
   
 //  renders out all of the book cards
   return (
-    <Container>
-    <Row className="row">
- <Col className="col" size="sm-12">
-
-<div className="card mb-3">
-<div className="row no-gutters">
-  <div className="col-md-4">
-  <img src={props.image} className="card-img" alt="" />
-  </div>
-  <div className="col-md-8">
-    <div className="card-body">
-      <h5 className="card-title">{props.title}</h5>
-      <p className="card-text">Author: {props.author}</p>
-      <p className="card-text">{props.description}</p>
-      <div id="wrapper">
-      <button className="saveBook btn btn-primary" id={props.id} onClick={props.handleSavedButton}> 
-                                            Save Book
-                                        </button>
-                                        <a href={props.view} target="_blank" />
-                                            <button className="viewBook btn btn-success">
-                                                View Book
-                                        </button>
-    
-    </div>
-  </div>
-</div>
-</div>
-
-</div>
-
-</Col>
-</Row>
-   <Row className="row">
-   <Col className="col" size="sm-12">
 
     <div className="list">
         {/* <Row className="row">
@@ -51,7 +16,7 @@ const BookList = (props) => {
             <h3>Results</h3>
         </Col>
         </Row> */}
-        {/* {
+        {
             // contains prop called books which will contain data of the state of books and then we map over that book data, for every object for that array were going to create a book card component
             // But what if the props object does not have the books property initially?
             // So in that case we use the ? operation also known as optional chaining
@@ -71,12 +36,10 @@ const BookList = (props) => {
                 />
                 
             })
-        } */}
+        }
 </div>
    
-    </Col>
-    </Row>
-    </Container>
+    
   );
 }
 
