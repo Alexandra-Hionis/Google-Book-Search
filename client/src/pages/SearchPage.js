e9a842fac1361c9f5d15cfe18a547e7d6a97229b
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-02-06 15:41:26 -0500

    fixed heroku error by adding type to string in models book.js file

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -69,6 +69,16 @@ class SearchPage extends Component {
             .catch(err => console.log(err))
     }
 
+//     handleSavedButton = event => {
+//         // console.log(event)
+//         event.preventDefault();
+//         console.log(this.state.books)
+//         let savedBooks = this.state.books.filter(book => book.id === event.target.id)
+//         savedBooks = savedBooks[0];
+//         API.saveBook(savedBooks)
+//         .then(this.setState({ message: alert("Your book is saved") }))
+//         .catch(err => console.log(err))
+// }
 
     render() {
         console.log(this.state.books)
--- models/book.js
+++ models/book.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  description: {String, required: true },
+  description: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
 
