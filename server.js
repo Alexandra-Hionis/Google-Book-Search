b333043fd44c6a92c9c927e248ceee51f9260e47
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-02-06 00:14:47 -0500

    still trying to fix heroku error

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
@@ -6,21 +5,31 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Define middleware here
+// define middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
-app.use(routes)
+// define api routes 
+// (within this file, if no API routes are hit, the React app is sent)
+app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
+// connect to mongodb
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
+// start server
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
