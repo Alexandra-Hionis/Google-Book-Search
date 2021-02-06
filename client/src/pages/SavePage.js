0dfe843211b1d6f1cf17500ec1804b887be989a3
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-02-06 15:26:07 -0500

    commented out saved files

--- client/src/pages/SavePage.js
+++ client/src/pages/SavePage.js
@@ -1,41 +1,41 @@
-import React, {Component} from "react";
-import SavedBookList from "../components/SavedBookList.js";
-import Container from "../components/Container.js";
+// import React, {Component} from "react";
+// import SavedBookList from "../components/SavedBookList.js";
+// import Container from "../components/Container.js";
 
-import API from '../utils/API'
+// import API from '../utils/API'
 
 
-class SaveBook extends Component {
-    state = {
-        savedBooks: []
-    };
+// class SaveBook extends Component {
+//     state = {
+//         savedBooks: []
+//     };
 
-    //when this component mounts, grab all books that were save to the database 
-    componentDidMount() {
-        API.getBooks()
-            .then(res => this.setState({ savedBooks: res.data }))
-            .catch(err => console.log(err))
-    }
+//     //when this component mounts, grab all books that were save to the database 
+//     componentDidMount() {
+//         API.getBooks()
+//             .then(res => this.setState({ savedBooks: res.data }))
+//             .catch(err => console.log(err))
+//     }
 
-    //function to remove book by id
-    handleDeleteButton = id => {
-        API.deleteBook(id)
-            .then(res => this.componentDidMount())
-            .catch(err => console.log(err))
-    }
+//     //function to remove book by id
+//     handleDeleteButton = id => {
+//         API.deleteBook(id)
+//             .then(res => this.componentDidMount())
+//             .catch(err => console.log(err))
+//     }
 
-    render() {
-        return (
-            <Container >
+//     render() {
+//         return (
+//             <Container >
                 
                 
-                    <SavedBookList savedBooks={this.state.savedBooks} handleDeleteButton={this.handleDeleteButton} />
+//                     <SavedBookList savedBooks={this.state.savedBooks} handleDeleteButton={this.handleDeleteButton} />
                 
-            </Container>
-        )
-    }
-}
+//             </Container>
+//         )
+//     }
+// }
 
 
 
-export default SaveBook 
+// export default SaveBook 
